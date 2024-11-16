d1d755c6a177f96c20a0edd0de25d3e0ffe15893
qwertyuiopaasdf <sahiruhaan4@gmail.com>
2024-11-17 02:22:06 +0400

    new changes since last oens didnt update

--- reactjs-/src/App.jsx
+++ reactjs-/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import myImage from "./Images/BC_logo.png"
 import './App.css'
 
 function App() {
@@ -9,14 +10,16 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://sites.google.com/gemsdaa.net/daa-clubs-and-activities?authuser=0" target="_blank">
+          <img src={myImage} className="logo" alt="BC Logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>
+        BC Study Guide
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +29,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Aimed at providing you with the best education possible
       </p>
     </>
   )
